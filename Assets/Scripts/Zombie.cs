7e97cc79f61e3cdea677d49033612b6beea7c7f3
finlaymcdonald <joseph.mcdonald@hammerheadvr.com>
2016-02-29 20:53:07 +0000

    added more to zombie AI. Now gravitate to the closest building

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -8,19 +8,45 @@ public class Zombie : MonoBehaviour {
 	private int damage;
 	private BoxCollider body;
 	public Transform towerTransform;
+	public Building[] buildings;
 
 	// Use this for initialization
 	void Start () {
 		health = 3;
 		body = gameObject.GetComponent<BoxCollider> ();
 		speed = 1;
-		towerTransform = GameObject.Find ("WatchTower").transform;
+		buildings = FindObjectsOfType (typeof(Building)) as Building[];
+		Debug.Log (buildings.Length);
+		//towerTransform = GameObject.Find ("WatchTower").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float step = speed * Time.deltaTime;
+		
+		if(towerTransform == null){ //check if it has a target
+			findClosestBuilding();
+		}
+		
+		float step = speed * Time.deltaTime; //move towards the closest buidling
 		transform.position = Vector3.MoveTowards (transform.position, towerTransform.position, step);
+	}
+
+	/*
+	 * Finds and sets the closest building for the zombie to attack
+	 **/
+	public void findClosestBuilding(){
+		float minDistance = Mathf.Infinity;
+		Vector3 currentPos = transform.position;
+
+		foreach(Building b in buildings){ //iterate through all buildings to find the closest
+			Transform t = b.transform;
+			float dist = Vector3.Distance (currentPos, t.position);
+
+			if (dist < minDistance) { //set the closest building
+				minDistance = dist;
+				towerTransform = t;
+			}
+		}
 	}
 
 	public void setSpeed(int newSpeed){
