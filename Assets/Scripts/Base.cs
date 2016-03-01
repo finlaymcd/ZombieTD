e053617eeb1a36702fd8b512f5df8d7a26e5bd42
finlaymcd <joseph.mcdonald@hammerheadvr.com>
2016-03-01 10:12:10 +0000

    added base script

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Base : Building {
+
+	// Use this for initialization
+	void Start () {
+		maxHealth = 15;
+		capacity = 5;
+		health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
