e86651287953e487e31f22065694fe770026b6d5
finlaymcd <joseph.mcdonald@hammerheadvr.com>
2016-03-03 17:54:59 +0000

    Got touch drag working in 3D, and sort of got it to play nice with the snap to grid. Snap to grid needs work. LEE IF YOU'RE READING THIS, I think my maths might be wrong. If you wanted to take a look that would be helpful. It's in the positionalRounding script. I'm trying to snap the items I attach it to, to 0.25 unit increments. It's the rePosition() method.

--- Assets/Scripts/PositionalRounding.cs
+++ Assets/Scripts/PositionalRounding.cs
@@ -13,7 +13,9 @@ public class PositionalRounding : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		if (Input.GetButtonDown("Fire1")) {
+			rePosition ();
+		}
 	}
 
 	public void rePosition(){
---
+++ Assets/Scripts/Selector.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selector : MonoBehaviour {
+
+	private PositionalRounding tower;
+	private Vector3 castPos;
+	public RaycastHit hit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) {
+				print (hit.point);
+				castPos = new Vector3 (hit.point.x, 11.0f, hit.point.z);
+				if (hit.collider.gameObject.GetComponent<PositionalRounding> () != null) {
+					tower = hit.collider.gameObject.GetComponent<PositionalRounding> ();
+					//tower.rePosition (castPos);
+					tower.transform.position = castPos;
+				}
+
+			}
+		}
+		if(Input.GetMouseButtonUp(0)){
+			if(tower != null){
+			tower.rePosition ();
+			}
+			tower = null;
+		}
+	}
+
+
+
+}
