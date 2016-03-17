247d5dac16c70cb4875fc4f63d5b64de92732342
finlaymcd <joseph.mcdonald@hammerheadvr.com>
2016-03-17 12:27:16 +0000

    cleaning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -45,17 +45,14 @@ public class GameManager : MonoBehaviour {
 		Debug.Log ("called");
 		foreach(Spawner spawn in spawners){
 			if(spawn == s){
+				Debug.Log (s.transform.position);
 				Debug.Log ("spawn = s");
 				if(spawn.onXaxis && spawn.transform.position.x < 0){ //south side
 					Debug.Log("south");
 					Vector3 newPos = new Vector3(spawn.transform.position.x - expansion, spawn.transform.position.y, spawn.transform.position.z);
 					s.transform.position = newPos;
 				}
-				if(spawn.onXaxis && spawn.transform.position.x > 0){ // north side
-					Debug.Log("north");
-					Vector3 newPos = new Vector3(spawn.transform.position.x + expansion, spawn.transform.position.y, spawn.transform.position.z);
-					s.transform.position = newPos;
-				}
+			
 				if(spawn.onXaxis == false && spawn.transform.position.z < 0){ // west side
 					Debug.Log("west");
 					Vector3 newPos = new Vector3(spawn.transform.position.x + expansion, spawn.transform.position.y, spawn.transform.position.z);
@@ -64,6 +61,11 @@ public class GameManager : MonoBehaviour {
 				if (spawn.onXaxis == false && spawn.transform.position.z > 0) { // east side (motherfucker)
 					Debug.Log("east");
 					Vector3 newPos = new Vector3(spawn.transform.position.x - expansion, spawn.transform.position.y, spawn.transform.position.z);
+					s.transform.position = newPos;
+				}
+				else { // north side
+					Debug.Log("north");
+					Vector3 newPos = new Vector3(spawn.transform.position.x, spawn.transform.position.y, spawn.transform.position.z - expansion);
 					s.transform.position = newPos;
 				}
 			}
